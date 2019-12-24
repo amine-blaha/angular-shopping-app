089b2b8e0fef81f6bf7b41483abb8c859f25d41e
DeathDoctor <deathdoctor4499@gmail.com>
2019-12-24 17:52:55 +0100

    object models creation and components positioning

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("a test recipe", "this is a test", "https://www.google.com/search?q=recipe&sxsrf=ACYBGNRDqPy4bbsHfjejdZqOOox6TzGELQ:1577202188245&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjujq2B0M7mAhWuxoUKHd8wDwAQ_AUoAnoECA8QBA&biw=1777&bih=841#imgrc=JiQfKKpComMwLM:")
+  ];
 
   constructor() { }
 
